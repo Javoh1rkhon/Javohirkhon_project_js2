07e431b60e5337cee8a5a45563f52f1828a57ca2
javoh1rkhon <rustamovjavohirkhon@gmail.com>
2023-10-07 14:58:33 +0500

    Homework_JS

---
+++ main.js
@@ -0,0 +1,157 @@
+// if else va vaga va yokiga 10ta mashq
+
+// N1 
+
+// let a = confirm("Isming Javohirmi?")
+
+// if (a) {
+//     console.log("True");
+// } else {
+//     console.log("False");
+// }
+
+// N2
+
+// let b = confirm("Isming Javohirmi?")
+
+// if (b) {
+//     let d = prompt("Unda familyangni yoz")
+//     let e = "Rustamov"
+//     if (d.trim() === e) {
+//         console.log("Ro'yxatdan o'ting");
+//     } else {
+//         console.log("false");
+//     }
+// } else {
+//     console.log("false");
+// }   
+
+// N3
+
+// let name = "Javohir"
+// let year = "13"
+
+// let f = prompt("Ismingni kirit")
+
+// if (f.trim() === name) {
+//     console.log("true");
+//     let g = prompt("Nechi yoshdasan")
+//     if (g.trim() === year) {
+//         console.log("true");
+//     } else {
+//         console.log("false");
+//     }
+// } else {
+//     console.log("false");
+// }
+
+// N4
+
+// let userName = "Javoh1rkhon"
+// let password = "777777"
+
+// let a = prompt("Usernameni kiriting")
+// let b = prompt("Passwordni kiriting")
+
+// if (a.trim() === userName && b.trim() === password) {
+// console.log("true");
+// } else {
+// console.log("XXX");
+//     }
+
+// N5
+
+// let balance = "100000"
+// let  passwordCard = "112233"
+// let parol = "123456"
+
+// let a = prompt("Balansizda nechi pul bor")
+
+// if (a === balance) {
+//     console.log(true);
+
+//     let b = prompt("Karta paroli nima?")
+//     if (b === passwordCard || b === parol) {
+//         console.log("Hammasi chotki");
+//     } 
+// } else {
+//     console.log(false);
+// }
+
+
+// N6
+
+// let a = prompt("Ismingni kirit")
+
+// if (a.trim()) {  
+//     console.log("true");
+//     let b = prompt("Endi familyangni kirit")
+//     if (b.trim()) {
+//         console.log("true");
+//     } else {
+//         console.log("false");
+//     }
+// } else {
+//     console.log("false");
+// }
+
+
+// N7
+
+// let a = 20
+// let b = 5
+// let d = 100
+
+// if (a * b && d) {
+//     console.log("true");
+// } else {
+//     console.log("false");
+// }
+
+// N8
+
+// let a = 100
+// let b = 100
+// let d = 10000
+
+// if (a * b && d) {
+//     console.log("true");
+//     let e = prompt("100 + 100 = ?")
+//     let f = "200"
+//     if (e === f) {
+//         console.log("Ekzamendan o'tding");
+//     } else {
+//         console.log("O'tmading");
+//     }
+// } else {
+//     console.log("Yiqilgan");
+// }
+
+// N9
+
+// let a = prompt("200+400?")
+// let b = "600"
+
+// if (a.trim() === b) {
+//     console.log("true");
+//     let f = "800"
+//     let g = prompt("400+400=?")
+//     if (f === g) {
+//         console.log("Hammasi chotki");
+//     } else {
+//         console.log("XXX");
+//     }
+// } else {
+//     console.log("XXX");
+// }
+
+// N10
+
+// let a = 10
+// let b = 15
+
+// if (a === 10 || b === 15) {
+//     console.log("true");
+// } else {
+//     console.log("false");
+// }
